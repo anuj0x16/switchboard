6906a4192cc4deb1b0e4981a4377b92bdb28b711
Anuj Kutal <anujkutal2004@gmail.com>
2025-11-28 21:50:05 +0530

    add database connection setup for postgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/anuj0x16/switchboard/internal/env"
+	"github.com/jackc/pgx/v5/pgxpool"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 type config struct {
-	httpPort int
+	httpPort    int
+	databaseUrl string
 }
 
 type application struct {
@@ -21,10 +25,20 @@ type application struct {
 
 func main() {
 	cfg := config{
-		httpPort: env.GetInt("HTTP_PORT", 4000),
+		httpPort:    env.GetInt("HTTP_PORT", 4000),
+		databaseUrl: env.GetString("DATABASE_URL", ""),
 	}
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	logger.Info("database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -39,9 +53,24 @@ func main() {
 
 	logger.Info("starting http server", "port", cfg.httpPort)
 
-	err := srv.ListenAndServe()
-	if err != nil {
+	if err := srv.ListenAndServe(); err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+func openDB(cfg config) (*pgxpool.Pool, error) {
+	db, err := pgxpool.New(context.Background(), cfg.databaseUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.Ping(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
