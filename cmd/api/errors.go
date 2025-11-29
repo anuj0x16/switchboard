7f9732701fe6575b0ba45834e2046abdc31b2df5
Anuj Kutal <anujkutal2004@gmail.com>
2025-11-29 11:57:39 +0530

    minor change

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func (app *application) sendErrorResponse(w http.ResponseWriter, r *http.Request, status int, msg any) {
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, msg any) {
 	err := app.writeJSON(w, status, payload{"error": msg})
 	if err != nil {
 		app.logger.Error(err.Error(), "method", r.Method, "url", r.URL.String())
@@ -16,23 +16,23 @@ func (app *application) sendErrorResponse(w http.ResponseWriter, r *http.Request
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
 	app.logger.Error(err.Error(), "method", r.Method, "url", r.URL.String())
 	msg := "the server encountered a problem and could not process your request"
-	app.sendErrorResponse(w, r, http.StatusInternalServerError, msg)
+	app.errorResponse(w, r, http.StatusInternalServerError, msg)
 }
 
 func (app *application) notFound(w http.ResponseWriter, r *http.Request) {
 	msg := "the requested resource could not be found"
-	app.sendErrorResponse(w, r, http.StatusNotFound, msg)
+	app.errorResponse(w, r, http.StatusNotFound, msg)
 }
 
 func (app *application) methodNotAllowed(w http.ResponseWriter, r *http.Request) {
 	msg := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
-	app.sendErrorResponse(w, r, http.StatusMethodNotAllowed, msg)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, msg)
 }
 
 func (app *application) badRequest(w http.ResponseWriter, r *http.Request, err error) {
-	app.sendErrorResponse(w, r, http.StatusBadRequest, err.Error())
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
 
 func (app *application) failedValidation(w http.ResponseWriter, r *http.Request, errors map[string]string) {
-	app.sendErrorResponse(w, r, http.StatusUnprocessableEntity, errors)
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
