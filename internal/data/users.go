6b5c8956566b50ef3c31d9f617e0a3880b6cc61e
Anuj Kutal <anujkutal2004@gmail.com>
2025-11-29 11:11:30 +0530

    add users model

---
+++ internal/data/users.go
@@ -0,0 +1,70 @@
+package data
+
+import (
+	"errors"
+	"time"
+
+	"github.com/anuj0x16/switchboard/internal/validator"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        string    `json:"id"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+func (p *password) Set(plaintext string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintext), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintext
+	p.hash = hash
+
+	return nil
+}
+
+func (p *password) Match(plaintext string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintext))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Match(validator.EmailRe, email), "email", "must be valid email address")
+}
+
+func ValidatePassword(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be atleast 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	ValidateEmail(v, user.Email)
+
+	if user.Password.plaintext != nil {
+		ValidatePassword(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
+}
